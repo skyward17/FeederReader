548304afa00592e03194c27cad944f59eaffea0f
Geoff <skyward17@outlook.com>
2018-05-01 10:54:06 -0500

    feat: Added 'New Feed Selection' Test Suite framework

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -84,6 +84,7 @@ describe('The MENU', function() {
 
     /* TODO: Write a new test suite named "Initial Entries" */
 describe('Initial Entries', function() {
+    let
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
@@ -91,11 +92,15 @@ describe('Initial Entries', function() {
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
 
+});  // End describe 'Initial Entries'
+
+
     /* TODO: Write a new test suite named "New Feed Selection" */
+describe('New Feed Selection', function() {
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
-});  // End describe 'Initial Entries'
+});  // End describe 'New Feed Selection'
 }());
